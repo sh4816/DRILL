ab7eb205943e40c75da4860e50b7a42c5b3aae20
sh4816 <hpy469@naver.com>
2021-09-09 20:26:51 +0900

    DRILL 03 완성
    2018182009 김승환

---
+++ 03/name.py
@@ -0,0 +1,116 @@
+import turtle
+# 2018182009 김승환 - DRILL 03
+# 김
+turtle.penup()
+turtle.goto(-350, 300)
+# ㄱ
+turtle.pendown()
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(150)
+# ㅣ
+turtle.penup()
+turtle.goto(-220, 310)
+turtle.left(45)
+
+turtle.pendown()
+turtle.forward(120)
+# ㅁ
+turtle.penup()
+turtle.goto(-320, 160)
+turtle.left(90)
+
+turtle.pendown()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(60)
+#
+# 승
+turtle.penup()
+turtle.goto(-50, 300)
+# ㅅ
+turtle.pendown()
+turtle.left(150)
+turtle.forward(100)
+turtle.goto(-50,300)
+turtle.left(60)
+turtle.forward(100)
+# ㅡ
+turtle.penup()
+turtle.goto(-130, 190)
+
+turtle.pendown()
+turtle.left(60)
+turtle.forward(165)
+# ㅇ
+turtle.penup()
+turtle.goto(-50, 90)
+
+turtle.pendown()
+turtle.circle(38)
+#
+# 환
+turtle.penup()
+turtle.goto(110, 310)
+# ㅎ
+turtle.pendown()
+turtle.right(20)
+turtle.forward(70)
+
+turtle.penup()
+turtle.goto(95, 280)
+
+turtle.pendown()
+turtle.left(20)
+turtle.forward(100)
+
+turtle.penup()
+turtle.goto(147, 220)
+
+turtle.pendown()
+turtle.circle(27)
+# ㅗ
+turtle.penup()
+turtle.goto(147, 215)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(25)
+
+turtle.penup()
+turtle.goto(95, 190)
+
+turtle.pendown()
+turtle.left(90)
+turtle.forward(110)
+# ㅏ
+turtle.penup()
+turtle.goto(210, 320)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(160)
+
+turtle.penup()
+turtle.goto(210, 240)
+
+turtle.pendown()
+turtle.left(90)
+turtle.forward(50)
+# ㄴ
+turtle.penup()
+turtle.goto(135, 170)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(100)
+
+
+turtle.exitonclick()
